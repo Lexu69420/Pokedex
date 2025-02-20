5d2e17c56915bf923ffc2e7d0992876af219b5dc
Lexuuu11 <jepoylao@gmail.com>
2025-02-20 16:05:06 +0800

    API and frontend

---
+++ routes/pokemon.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const axios = require('axios');
+const dotenv = require("dotenv");
+dotenv.config();
+
+const router = express.Router();
+const API_URL = process.env.API_URL;
+
+router.get("/", async (req, res) => {      
+    try {
+        const response = await axios.get(`${API_URL}/pokemon`);
+        return res.render("page/home", {pokeonList: response.data.results});
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({
+            message: "Internal Server Error",
+        });
+    }
+});
+
+router.get("/pokemon/:name", async (req, res) => {
+    try {
+        const { name } = req.params;
+        const response = await axios.get(`${API_URL}/${name}`);
+        return res.render("page/detail", {pokemon: response.data});
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({
+            message: "Internal Server Error",
+        });
+    }
+});
+
+router.get("/search", async (req, res) => {
+    try {
+        const { name } = req.query;
+        if (!name) return res.redirect("/");
+        const response = await axios.get(`${API_URL}/${name.toLowerCase()}`);
+        return res.render("page/detail", {pokemon: response.data});
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({
+            message: "Internal Server Error",
+        });
+    }
+});
+
+module.exports = router;
